261ea6cc497e23c9c6f3d613994c4c26cd0a2430
KantyshVitali <kantyshvitali@gmail.com>
2021-02-22 17:24:45 +0300

    feat: learn REST api basics on express and vue.js

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
-const express = require('express')
-const path = require('path')
-const {v4} = require('uuid')
-const app = express()
+const express = require('express');
+const path = require('path');
+const {v4} = require('uuid');
+const app = express();
 
 let CONTACTS = [
-  {id: v4(), name: 'Владилен', value: '+7-921-100-20-30', marked: false}
+	{	id: v4(), name: "Vitali", value: "+375-xx-xxx-xx-xx", noted: false },
 ]
 
-app.use(express.json())
+app.use(express.json());
 
-// GET
+//GET
 app.get('/api/contacts', (req, res) => {
-  setTimeout(() => {
-    res.status(200).json(CONTACTS)
-  }, 1000)
-})
+	res.status(200).json(CONTACTS);
+});
 
-// POST
+//POST
 app.post('/api/contacts', (req, res) => {
-  const contact = {...req.body, id: v4(), marked: false}
-  CONTACTS.push(contact)
-  res.status(201).json(contact)
+	const contact = {...req.body, id: v4(), noted: false};	
+	CONTACTS.push(contact);
+	res.status(201).json(contact);
 })
 
-// DELETE
+//DELETE 
 app.delete('/api/contacts/:id', (req, res) => {
-  CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
-  res.status(200).json({message: 'Контакт был удален'})
+	CONTACTS = CONTACTS.filter(x => x.id !== req.params.id);
+	res.status(200).json({message: 'Contact was Deleted'});
 })
 
-// PUT
+//PUT
 app.put('/api/contacts/:id', (req, res) => {
-  const idx = CONTACTS.findIndex(c => c.id === req.params.id)
-  CONTACTS[idx] = req.body
-  res.json(CONTACTS[idx])
+	const putIndex = CONTACTS.findIndex(c => c.id === req.params.id);
+	CONTACTS[putIndex] = req.body;
+	res.status(200).json(CONTACTS[putIndex]);
 })
 
 
-app.use(express.static(path.resolve(__dirname, 'client')))
+app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
-})
+	res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+});
 
-app.listen(3000, () => console.log('Server has been started on port 3000...'))
+app.listen(8000, () => console.log('Server has been started on port 8000...'));
--- client/frontend.js
+++ client/frontend.js
@@ -1,80 +1,88 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
-
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
 
 Vue.component('loader', {
-  template: `
-    <div style="display: flex;justify-content: center;align-items: center">
-      <div class="spinner-border" role="status">
-        <span class="sr-only">Loading...</span>
-      </div>
-    </div>
-  `
+	template: `
+		<div style="display: flex; justify-content: center; align-items: center">
+			<div class="spinner-border" role="status">
+				<span class="sr-only">Loading...</span>
+			</div>
+		</div>
+	`
 })
 
 new Vue({
-  el: '#app',
-  data() {
-    return {
-      loading: false,
-      form: {
-        name: '',
-        value: ''
-      },
-      contacts: []
-    }
-  },
-  computed: {
-    canCreate() {
-      return this.form.value.trim() && this.form.name.trim()
-    }
-  },
-  methods: {
-    async createContact() {
-      const {...contact} = this.form
+	el:"#app",
+	data() {
+		return {
+			loading: false,
+			form : {
+				name: '',
+				value: '',
+			},
+			contacts: []
+		}
+	},
+	computed: {
+		canCreate() {
+			return this.form.name.trim() && this.form.value.trim();
+		},
+	},
+	methods: {
+		async createContact() {
+			const {...contact} = this.form;
 
-      const newContact = await request('/api/contacts', 'POST', contact)
+			console.log(contact);
 
-      this.contacts.push(newContact)
+			const newContact = await request('/api/contacts', 'POST', contact);
 
-      this.form.name = this.form.value = ''
-    },
-    async markContact(id) {
-      const contact = this.contacts.find(c => c.id === id)
-      const updated = await request(`/api/contacts/${id}`, 'PUT', {
-        ...contact,
-        marked: true
-      })
-      contact.marked = updated.marked
-    },
-    async removeContact(id) {
-      await request(`/api/contacts/${id}`, 'DELETE')
-      this.contacts = this.contacts.filter(c => c.id !== id)
-    }
-  },
-  async mounted() {
-    this.loading = true
-    this.contacts = await request('/api/contacts')
-    this.loading = false
-  }
+			console.log(newContact);
+			this.contacts.push(newContact);
+			this.clearAll();
+		},
+		clearAll() {
+			this.form.name = '';
+			this.form.value = '';
+		},
+		async markContact(id) {
+			const contact = this.contacts.find(x => x.id === id);
+			const updatedContact = await request(`/api/contacts/${id}`, 'PUT', {
+				...contact,
+				noted: true,
+			});
+			contact.noted = updatedContact.noted;
+		},
+		async	removeContact(id) {
+			await request(`/api/contacts/${id}`, 'DELETE')
+			this.contacts = this.contacts.filter(x => x.id !== id);
+		}
+	},
+	async mounted() {
+		this.loading = true;
+		this.contacts = await request('/api/contacts');
+		this.loading = false;
+	}
 })
 
 async function request(url, method = 'GET', data = null) {
-  try {
-    const headers = {}
-    let body
+	try {
+		const headers = {};
+		let body;
 
-    if (data) {
-      headers['Content-Type'] = 'application/json'
-      body = JSON.stringify(data)
-    }
+		if(data) {	
+			headers['Content-Type'] = 'application/json';
+			body = JSON.stringify(data);
+		}
 
-    const response = await fetch(url, {
-      method,
-      headers,
-      body
-    })
-    return await response.json()
-  } catch (e) {
-    console.warn('Error:', e.message)
-  }
+		const response = await fetch(url, {
+			method,
+			headers,
+			body
+		})
+		return response.json();
+
+	} catch(e) {
+		console.warn('Error:', e.message);
+	} finally {
+		console.log('Final');
+	}
 }
